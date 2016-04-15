aca0f407409788f92d7af421fa46075d3338d138
Diego e Bruno <=>
2016-04-14 21:59:15 -0300

    user create categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,26 @@
+# Isso eh a classe CategoriesController
 class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to @category
+    else
+      flash[:alert] = 'Warning! All fields are mandatory.'
+      render :new
+    end
+  end
+end
+
+private
+
+def category_params
+  params.require(:category).permit(:name)
 end
