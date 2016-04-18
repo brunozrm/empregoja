b9173ccd461d651b35c7ff126bd48483ac8644cd
Diego e Bruno <=>
2016-04-18 20:39:57 -0300

    recent jobs

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,11 @@
+# asdsasfda
 class Job < ActiveRecord::Base
   belongs_to :company
   belongs_to :category
 
   validates :title, :category, :description, :location, presence: true
+
+  def recent?
+    created_at > 5.days.ago
+  end
 end
